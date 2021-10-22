1e0c6cc3897097d5cb8da422ebc91d4f93a0c646
as26uk <ajz@live.co.uk>
2021-10-22 15:28:36 +0100

    while for test

---
+++ while.py
@@ -0,0 +1,11 @@
+i = 1
+x = "_"
+while i < 2:
+    i = i + 1
+    x = x + "_"
+
+my_fruit = ["Apple", "Banana", "Orange", "peach", "apricot"]
+
+for i in my_fruit:
+    print(i.upper())
+    print("-" * len(i))
